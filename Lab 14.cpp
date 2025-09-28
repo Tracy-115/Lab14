0375307ee84ff8eef746ebc6523fb636f846a43e
Tracy-115 <kmin984@insite.4cd.edu>
2025-09-28 14:03:58 -0700

    Update Lab 14.cpp

--- Lab 14.cpp
+++ Lab 14.cpp
@@ -1,1 +1,31 @@
+// COMSC-210 | Lab 14 | Kyawt Thinzar Min
+// IDE used: Visual Studio Code
+
+#include <iostream>
+using namespace std;
+
+class Color{
+  private:
+    int red;
+    int green;
+    int blue;
+  public:
+    void setRed (int r); {red = r;}
+    int getRed();        {return red};
+
+    void setBlue (int b); {blue = b;}
+    int getBlue();        {return blue;}
+
+    void setGreen (int g); {green = g;}
+    int getGreen();        {return green;}
+
+    void print(){
+        cout << setw(W15) << "Red: " << red << endl;
+        cout << setw(W15) << "Blue: " << blue << endl;
+        cout << setw(W15) << "Green: " << green << endl;
+    }
+};
+
+int main(){
+  
 
